9f9fc92cd8ebb72258fa2ccf119f9b9c6bfb702e
User <User@DESKTOP-1BB4VF4>
2021-07-27 10:46:24 +0900

    Second commit

---
+++ src/ch01_3_java_process/Hello.java
@@ -0,0 +1,20 @@
+package ch01_3_java_process;
+
+public class Hello {
+/*********************************************
+ *  작성자 : 정의찬
+ *  연락처 : 010-2470-3757
+ * 	작성일 : 2021-07-20
+ *  arg(입력) -> 메소드떈
+ *  출력 -> 메소드떈
+ *  내용 -> 메소드떈
+ *******************************************/
+	public static void main(String[] args) {
+		System.out.println(100+100);
+		System.out.println(200+200);
+	
+	}	
+
+}
+
+// main() -> entry point 
---
+++ src/ch01_3_java_process/RunStatmentExample.java
@@ -0,0 +1,22 @@
+package ch01_3_java_process;
+
+public class RunStatmentExample {
+
+	public static void main(String[] args) {
+		 
+		int x = 1;
+		int y = 2;
+		int result = x + y ;
+		
+		System.out.println(result);
+	/*
+	 * 두 수의 합을 출력
+	 * 두 수를 선언
+	 * 두 수의 합 연산
+	 * 연산 결과 저장
+	 * 저장된 값을 출력
+	 */
+		
+	} 
+
+}
